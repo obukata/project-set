2f3cbc13e83d5e2c61be61565d1943ac8e86f378
obukata <obukata@mj-inc.jp>
2017-12-08 17:34:11 +0900

    オリジナルモジュールについては一旦ペンディングで。

--- project-name_v1.0/assets/scripts/common.js
+++ project-name_v1.0/assets/scripts/common.js
@@ -1,8 +1,7 @@
-'use strict';
-
 // HTMLの読み込みが完全に完了したタイミングで発火
 // 画像の幅など取得する場合はこっち。
 window.addEventListener('load', function() {
+	'use strict';
 
 }, false);
 
@@ -11,11 +10,18 @@ window.addEventListener('load', function() {
 // DOMの読み込みが完了したタイミングで発火
 // loadイベントより早い。
 window.addEventListener('DOMContentLoaded', function() {
+	'use strict';
 
 }, false);
 
 
 
+// かといってもjQueryいきなり無くすのは難しいかと思うので
+// jQuery系統はここで起動させよう。
+$(function() {
+	'use strict';
+
+})
 
 
 
@@ -33,3 +39,31 @@ window.addEventListener('DOMContentLoaded', function() {
 // addEventListenerが、jQueryの$('').on('click', function() { });のようなものです。
 // element.addEventListener('click', function() { }, false);
 // element.addEventListener('resize', function() { }, false);
+
+
+// 無名関数、今まで
+// var f = function(a, b) {
+// 	return a + b;
+// };
+// console.log(f(2, 4)); -> 6
+
+// これからはアロー関数どう？
+// const g = (a, b) => {
+// 	return a + b;
+// };
+// console.log(g(2, 4)); -> 6
+
+// アロー関数その2
+// const h = (a, b) => a + b;
+// console.log(h(2, 4)); -> 6
+
+
+// 即時実行、今まで
+// (function() {
+// 	console.log("text");
+// });
+
+// アロー関数にて即時実行
+// (() => {
+// 	console.log("text");
+// });
